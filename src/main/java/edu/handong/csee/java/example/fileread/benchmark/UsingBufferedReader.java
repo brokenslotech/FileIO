ea35d7a4a3d5f4b787378818112e779b7ecdec1a
eugenehong <eugenehong@localhost>
2020-06-02 19:30:17 +0900

    first commit

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -8,12 +8,13 @@ import java.io.*;
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
 
-
+    	String fileName = args[0];
+    	
         //-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(fileName));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -26,7 +27,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        BufferedReader inputStream2= new BufferedReader(new FileReader(fileName));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
